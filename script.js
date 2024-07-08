50e5eac21ff761d9df2a9a904abe13780aed8066
Lawjay Lee <lawjaylee1@icloud.com>
2024-07-08 09:06:24 -0700

    got you lose

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function updateScore(target) {
         scoreTitle.innerText = "Current Score: " + curScore;
 
         if (curScore > 21) {
-            lostGame("bust");
+            endGame("bust");
         }
     }
     if (target === "dealer") {
@@ -128,7 +128,6 @@ async function resetGame() {
 
     startScreen.style.display = "none";
     main.style.display = "inline";
-    choices.style.display = "flex";
     outcome.style.display = "none";
     dealer.style.display = "inline";
 
@@ -140,6 +139,8 @@ async function resetGame() {
 
     await sleep(1500);
     addCard("dealer");
+
+    choices.style.display = "flex";
 }
 
 function sleep(ms) {
@@ -156,17 +157,19 @@ async function dealerTurn() {
     // notEnough = (dScore < curScore && dScore < 21);
     // dontWantTie = (dScore === curScore && dScore <= 16);
     // while ((dScore < curScore && dScore < 21) || (dScore === curScore && dScore <= 16)) {
-    while (dScore < curScore && dScore < 21) {
+    while (dScore <= curScore && dScore < 21) {
         await sleep(1500);
         addCard("dealer");
     }
 
     if (dScore > curScore) {
-        lostGame("lost");
+        endGame("lost");
     }
+
+    else if (curScore > dScore)
 }
 
-function lostGame(reason) {
+function endGame(reason) {
     const outcome = document.getElementById("outcome");
     const choices = document.getElementById("choices");
     choices.style.display = "none";
@@ -178,11 +181,18 @@ function lostGame(reason) {
         t1 = document.createElement("h1");
         t1.innerHTML = "BUST!!";
         t2 = document.createElement("h1");
-        t1.innerHTML = "You Lose!";
+        t2.innerHTML = "You Lose!";
         outcomeText.appendChild(t1);
         outcomeText.appendChild(t2);
     }
     else if (reason === "lost") {
+        t1 = document.createElement("h1");
+        t1.innerHTML = "Dealer Wins!";
+        outcomeText.appendChild(t1);
+
+        t2 = document.createElement("h1");
+        t2.innerHTML = "You Lose!";
+        outcomeText.appendChild(t2);
     }
 }
 

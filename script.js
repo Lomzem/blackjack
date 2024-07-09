905f694ffc61d575a16b45438362a1a2ec10f8fd
Lawjay Lee <lawjaylee1@icloud.com>
2024-07-09 11:45:42 -0700

    removed unnecessary discardedcards

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const cardsArray = ['4H.svg', '3D.svg', '9S.svg', '2H.svg', '6S.svg', 'KC.svg', 'TC.svg', '5H.svg', '3S.svg', '7D.svg', 'JC.svg', 'JD.svg', 'AS.svg', 'TH.svg', '8C.svg', '6C.svg', '2D.svg', 'KD.svg', '4D.svg', 'KH.svg', '9H.svg', '3H.svg', '9C.svg', 'AD.svg', 'QC.svg', '7S.svg', '9D.svg', 'AH.svg', '8D.svg', 'JS.svg', '6D.svg', 'KS.svg', '5S.svg', 'TD.svg', '4C.svg', 'QS.svg', '2S.svg', '7C.svg', 'QD.svg', 'JH.svg', '8S.svg', 'AC.svg', '4S.svg', 'TS.svg', '5C.svg', '2C.svg', '5D.svg', '8H.svg', '3C.svg', 'QH.svg', '7H.svg', '6H.svg']
 
-const discardedCards = [];
 const curCards = [];
 const dCards = [];
 
@@ -84,8 +83,7 @@ function addCard(target) {
     hand.push(cardsArray[randIndex]);
     updateScore(target);
 
-    let removedCard = cardsArray.splice(randIndex, 1)[0];
-    discardedCards.push(removedCard);
+    cardsArray.splice(randIndex, 1)[0];
 }
 
 async function resetGame() {
